9523dd1f93b42eac9b5aba01808bdf01f5b84fa3
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-06-08 14:01:53 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-2xl font-bold py-4 pl-6">The Task Tracker</h1>
+      <p>Hi There!</p>
+      <p>Click +New to add a new task</p>
+    </>
   );
 }
 
